80ea77b7a8d034855ebcf89c2916edc11d90b087
RatYoung <yhrnju@qq.com>
2018-05-10 20:39:09 +0800

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    noticeList: [
+      '欢迎使用本平台，使用前请务必阅读此公告'
+    ]
+  }
+
+})
