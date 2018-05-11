abcbc43daef43080aef91161aa6102e1ab2097d6
RatYoung <yhrnju@qq.com>
2018-05-11 22:25:54 +0800

    修改了首页代码
    添加了加载动画，对公告栏和商品种类栏进行了修改，其中商品种类栏还存在无法显示的bug。

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,52 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+var app = getApp()
 Page({
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    remind: '加载中',
+    noticeList:{
+      dataList: ['嘻嘻嘻', '嘤嘤嘤', '嘿嘿嘿']
+    },
+    categories: [
+      {id: 0, name: '全部'},
+      {id: 1, name: '美妆'},
+      {id: 2, name: '服饰'},
+      {id: 3, name: '食品'},
+      {id: 4, name: '门票'},
+      {id: 5, name: '其他'}
     ],
-    noticeList: [
-      '欢迎使用本平台，使用前请务必阅读此公告'
-    ]
-  }
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    loadingHidden: false, // loading
+    userInfo: {},
+    swiperCurrent: 0,
+    selectCurrent: 0,
+    categories: [],
+    activeCategoryId: 0,
+    goods: [],
+    scrollTop: "0",
+    loadingMoreHidden: true,
 
+    hasNoCoupons: true,
+    coupons: [],
+    searchInput: '',
+  },
+
+  onReady: function() {
+    var that = this;
+    setTimeout(function () {
+      that.setData({
+        remind: ''
+      });
+    }, 1000);
+  },
+
+  tabClick: function (e) {
+    this.setData({
+      activeCategoryId: e.currentTarget.id
+    });
+    this.getGoodsList(this.data.activeCategoryId);
+  }
 })
