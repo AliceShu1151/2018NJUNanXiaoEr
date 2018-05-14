fbd9addd07498638694ab61220cd9ffbad2efbd8
RatYoung <yhrnju@qq.com>
2018-05-14 12:33:01 +0800

    解决了获取用户信息的问题
    新增一个消息弹框，用于获取用户信息，使用open-data来展示头像（但头像是矩形，不知道怎么改成圆形，以后再去优化）

--- pages/start/start.js
+++ pages/start/start.js
@@ -3,6 +3,7 @@
 var app = getApp();
 Page({
   data: {
+    showModal: false,
     remind: '加载中',
     angle: 0,
     userInfo: {}
@@ -13,18 +14,48 @@ Page({
     });
   },
   onLoad: function () {
-    var that = this
+    var that = this;
     wx.setNavigationBarTitle({
       title: wx.getStorageSync('mallName')
-    })
-    app.getUserInfo(function (userInfo) {
-      that.setData({
-        userInfo: userInfo
-      })
-    })
+    });
   },
   onShow: function () {
 
+  },
+  showDialogBtn: function () {
+    this.setData({
+      showModal: true
+    })
+  },
+  /**
+  * 弹出框蒙层截断touchmove事件
+  */
+  preventTouchMove: function () {
+  },
+  /**
+   * 隐藏模态对话框
+   */
+  hideModal: function () {
+    this.setData({
+      showModal: false
+    });
+  },
+  /**
+   * 对话框取消按钮点击事件
+   */
+  onCancel: function () {
+    this.hideModal();
+  },
+  //获取用户信息
+  userInfoHandler: function(e) {
+    app.globalData.userInfo = e.detail.rawData;
+    console.log(app.globalData.userInfo);
+  },
+  /**
+   * 对话框确认按钮点击事件
+   */
+  onConfirm: function (e) {
+    this.hideModal();
   },
   onReady: function () {
     var that = this;
@@ -43,5 +74,6 @@ Page({
         });
       }
     });
+    that.showDialogBtn();
   }
 });
