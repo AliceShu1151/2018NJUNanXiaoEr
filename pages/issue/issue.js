be7667138a5d371150598029df7f528bcaa80e92
RatYoung <yhrnju@qq.com>
2018-05-17 15:58:39 +0800

    yhr:发布页的前端逻辑大部分改动，对渲染语言小部分修改

--- pages/issue/issue.js
+++ pages/issue/issue.js
@@ -2,8 +2,21 @@
 let app = getApp();
 Page({
   data: {
-    urlArr: [],
-    tempFilePaths: '',
+    /*
+    yhr 5-17：
+    在data中定义商品名、商品描述、商品价格、商品种类
+    方便在函数方法中修改
+    tempFilePaths应为列表而不是字符串，因为可能都是选择多个图片
+    isFull用来判断是否已选择9张图，并隐藏上传图片按钮
+    */
+    isFull: false,
+    product_name: '',
+    product_description: '',
+    product_price: null,
+    //化妆品种类是默认值
+    product_category: '化妆品',
+    //urlArr: [],
+    tempFilePaths: [],
     value: ['A'],
     items: [{ name: 'A', value: '化妆品' }, { name: 'B', value: '服饰装扮' }, { name: 'C', value: '食品饮料' }, { name: 'D', value: '演出门票' }, { name: 'E', value: '数码电子' }, { name: 'F', value: '其他' }]
 
@@ -19,28 +32,85 @@ Page({
   onModalClose() {
 
   },
-  bindSelectorChange(e){
 
-    this.setData({
-
-      value: e.detail.value
-
-    })
-
-  },
-
-  deleteImage: function (e) {
-    // 获取本地显示的图片数组
+  /*
+  yhr 5-17:
+  获取商品名函方法
+  */
+  productTitle: function(e) {
     let that = this;
-    let index = e.currentTarget.dataset.index;
-    let urlArr = that.data.urlArr;
-    urlArr.splice(index, 1);
     that.setData({
-      urlArr: urlArr
+      product_name: e.detail.value
     });
-    console.log(that.data.urlArr)
+    //console.log(that.data.product_name);
   },
 
+  /*
+  yhr 5-17:
+  获取商品描述方法
+  */
+  productDetail: function(e) {
+    let that = this;
+    that.setData({
+      product_description: e.detail.value
+    });
+    //console.log(that.data.product_description);
+  },
+
+  /*
+  yhr 5-17:
+  获取商品价格方法
+  */
+  productPrice: function(e) {
+    let that = this;
+    if(isNaN(Number(e.detail.value))){
+      wx.showModal({
+        title: '提示',
+        content: '价格输入必须为数字！',
+      });
+      that.setData({
+        product_price: null
+      });
+      console.log(that.data.product_price);
+    }
+    else{
+      that.setData({
+        product_price: Number(e.detail.value)
+      });
+      //console.log(that.data.product_price);
+    }
+  },
+
+  /*
+  yhr 5-17:
+  获取商品种类方法
+  */
+  bindCheckboxChange: function(e) {
+    let that = this;
+    console.log(e.detail.value);
+    if(e.detail.value.length != 0){
+    for(let i = 0; i < that.data.items.length; i++){
+      if (that.data.items[i].name == e.detail.value){
+        that.setData({
+          product_category: that.data.items[i].value
+        });
+      }
+    }
+    }
+    else{
+      that.setData({
+        product_category: '化妆品'});
+    }
+    //console.log(that.data.product_category);
+  },
+
+  //上传图片的方法
+  /*
+  yhr 5-17:
+  删除上传Loading框
+  删除wx.uploadFile，应该在提交方法中调用该功能更为合适
+  当已经选择9张图片时应隐藏上传按钮
+  */
   upImg: function () {
     let that = this;
     wx.chooseImage({
@@ -55,56 +125,70 @@ Page({
         that.setData({
           tempFilePaths: res.tempFilePaths
         });
-        // 启动上传等待中...  
-        wx.showToast({
-          title: '正在上传...',
-          icon: 'loading',
-          mask: true,
-          duration: 10000
-        })
-        let uploadImgCount = 0;
-        for (let i = 0, h = tempFilePaths.length; i < h; i++) {
-          urlArr.push(tempFilePaths[i]);
+        if(res.tempFilePaths.length == 9){
           that.setData({
-            urlArr: urlArr
-        });
-        console.log(urlArr);
-        wx.uploadFile({
-          url: "https://e663c7332cbc5d0c48349e5609048c99.bmobcloud.com",
-          filePath: tempFilePaths[i],
-          name: 'uploadfile_ant',
-          formData: {
-            'imgIndex': i
-          },
-          header: {
-            "Content-Type": "multipart/form-data"
-          },
-          success: function (res) {
-            uploadImgCount++;
-            let data = JSON.parse(res.data);
-            //服务器返回格式: { "Catalog": "testFolder", "FileName": "1.jpg", "Url": "https://test.com/1.jpg" }  
-            let productInfo = that.data.productInfo;
-            if (productInfo.bannerInfo == null) {
-              productInfo.bannerInfo = [];
-            }
-
-            //返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-            wx.uploadFile({
-              url: "https://e663c7332cbc5d0c48349e5609048c99.bmobcloud.com",
-              // url: 'http://www.website.com/home/api/uploadimg',
-              filePath: tempFilePaths[i],
-              name: 'file',
-
-              success: function (res) {
-                //打印
-                console.log(res.data)
-              }
-            })
-          }
-        }) 
+            isFull: true
+          });
         }
       }
     })
   },
+  
+  //删除已上传图片的方法
+  /*
+  yhr 5-17:
+  不使用urlArr（有错）
+  直接使用tempFilePaths
+  */
+  deleteImage: function (e) {
+    // 获取本地显示的图片数组
+    let that = this;
+    let index = e.currentTarget.dataset.index;
+    let imagePaths = that.data.tempFilePaths;
+    imagePaths.splice(index, 1);
+    that.setData({
+      tempFilePaths: imagePaths,
+      isFull: false
+    });
+    //console.log(that.data.tempFilePaths);
+  },
+
+  /*
+  yhr 5-17:
+  新增提交方法
+  为修改数据库的入口
+  */
+  submit: function() {
+    let that = this;
+    if(that.data.tempFilePaths.length == 0){
+      wx.showModal({
+        title: '提示',
+        content: '上传的图片数量至少为一张！',
+      });
+    }
+    else if (that.data.product_price == null || that.data.product_price <= 0){
+      wx.showModal({
+        title: '提示',
+        content: '请重新输入价格！',
+      });
+    }
+    else if(that.data.product_description == ''){
+      wx.showModal({
+        title: '提示',
+        content: '请输入商品描述！',
+      });
+    }
+    else if(that.data.product_name == ''){
+      wx.showModal({
+        title: '提示',
+        content: '请输入商品标题！',
+      })
+    }
+    else{
+      /*
+      对数据库的操作写在这里
+      */
+    }
+  }
 
 })
