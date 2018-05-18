c5725d5aa4870a583e6db0a3afe878a9d1cfcd65
RatYoung <yhrnju@qq.com>
2018-05-18 08:58:18 +0800

    yhr：点击个人信息编辑按钮跳转至个人信息编辑页面

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -18,11 +18,9 @@ Page({
 		}, 1000);
 	},
 
-	aboutUs: function () {
-		wx.showModal({
-			title: '关于我们',
-			content: '本系统基于开源小程序商城系统 https://github.com/EastWorld/wechat-app-mall 搭建，祝大家使用愉快！',
-			showCancel: false
-		})
-	}
+	toEdit: function() {
+    wx.navigateTo({
+      url: '../../pages/editUserInfo/editUserInfo',
+    })
+  }
 })
