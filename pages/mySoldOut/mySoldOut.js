7956deab366e9a59fda08685390ad745767c79d0
Blickwinkel <1196061706@qq.com>
2018-05-19 13:18:42 +0800

    yx: 我卖出的与db联动

--- pages/mySoldOut/mySoldOut.js
+++ pages/mySoldOut/mySoldOut.js
@@ -1,90 +1,101 @@
 // pages/mySoldOut/mySoldOut.js
 let app = getApp();
+let Bmob = app.globalData.Bmob;
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    userInfo: {},
-    saveHidden: true,
-    soldOut: [
-      { businessId: 0, name: "QAQ", price: 200, img: '../../images/goods01.png', clickTimes: 5, state: 1, active: false },
-      { businessId: 1, name: "QAQ", price: 200, img: '../../images/goods02.png', clickTimes: 6, state: 1, active: false }
-    ],
-    soldOutLength: 5,
-  },
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		userInfo: {},
+		saveHidden: true,
+		soldOut: [],
+		soldOutLength: 5,
+	},
 
-  toIssuePage: function () {
-    wx.reLaunch({
-      url: '../../pages/issue/issue',
-    });
-  },
+	toIssuePage: function () {
+		wx.reLaunch({
+			url: '../../pages/issue/issue',
+		});
+	},
 
-  clickTap: function(e) {
-    wx.navigateTo({
-      url: "/pages/good_details/good_details?businessId=" + this.data.soldOut[e.currentTarget.id].businessId
-    });
-  },
+	clickTap: function (e) {
+		let that = this;
+		wx.navigateTo({
+			url: "/pages/good_details/good_details?businessId=" + this.data.soldOut[e.currentTarget.id].objectId,
+		});
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function () {
-    this.setData({
-      soldOutLength: this.data.soldOut.length,
-      userInfo: app.globalData.userInfo
-    });
-    console.log(this.data.soldOutLength);
-  },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function () {
+		let that = this;
+		that.fetchGoods();
+	},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+	fetchGoods: function () {
+		let that = this;
+		let db = Bmob.Query("goods");
+		db.equalTo("seller", "==", app.globalData.userOpenId);
+		db.equalTo("state", "==", 2);
+		db.order("-updatedAt");
+		db.find().then(res => {
+			that.setData({ soldOutLength: res.length });
+			let soldOut = res;
+			that.setData({
+				soldOut: soldOut,
+			});
+		});
+	},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+	},
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+	},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
+
+	},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {
+
+	},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {
+
+	},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
+
+	}
 })
