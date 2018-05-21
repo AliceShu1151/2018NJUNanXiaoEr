79d63df5de593b2ff19825cd98efb604fdaff4d5
unknown <1151545191@qq.com>
2018-05-21 22:35:06 +0800

    卖家信息页面

--- pages/sellerInfo/sellerInfo.js
+++ pages/sellerInfo/sellerInfo.js
@@ -1,12 +1,24 @@
+let app = getApp()
+let Bmob = app.globalData.Bmob;
+
 Page({
   data: {
-   seller: ''
+    userInfo: {},
+    seller: ''
   },
 
-  onLoad: function(options) {
-    var that = this;
-    that.setData({
-      seller: options.seller
-    })
-  }
+  onLoad(options) {
+    let that = this;
+    var seller = options.seller;
+    console.log(seller);
+    let db = Bmob.Query("_User");
+    db.equalTo("username", "==", seller);
+    db.find().then(res => {
+    // db.get(seller).then(res => {
+      that.setData({
+        userInfo: res[0]
+      });
+      console.log(that.data.userInfo);
+    });
+  },
 })
